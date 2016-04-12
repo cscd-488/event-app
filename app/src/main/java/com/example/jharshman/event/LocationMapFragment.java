c77a64d0a99ad25554fa3e387eee5996073a0a34
ayoung1 <aaron.young.0015@edmail.edcc.edu>
2016-04-11 19:43:33 -0700

    Cleanup of comments

--- app/src/main/java/com/example/jharshman/event/LocationMapFragment.java
+++ app/src/main/java/com/example/jharshman/event/LocationMapFragment.java
@@ -535,16 +535,6 @@ public class LocationMapFragment extends Fragment implements OnMapReadyCallback{
         }
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         void onFragmentInteraction(Uri uri);
     }
