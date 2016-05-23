dc5165f7e307581fd297865cf0e9c7406d333643
bemehiser <bemehiser@gmail.com>
2016-05-23 12:56:40 -0700

    Fixed the web api data parser tags to pull in double for lat and lon, and int for status. Removed Card class. Removed constructor from CheckPoint class. Fixed an issue in LocationMapFragment which caused a Null Pointer Exception.

--- app/src/main/java/com/example/jharshman/event/CheckPoint.java
+++ app/src/main/java/com/example/jharshman/event/CheckPoint.java
@@ -49,8 +49,7 @@ public class CheckPoint implements Comparable<CheckPoint>, Serializable {
     private String mTimeCreated;
     @SerializedName("updated_at")
     private String mTimeUpdated;
-    // todo get the data from the server
-//    @SerializedName("status")
+    @SerializedName("status")
     private int mChecked;
     private boolean mWasDisplayed;
 
